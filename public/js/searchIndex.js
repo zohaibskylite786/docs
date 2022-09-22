50dc1e196bfb1abc067811b5c30e1cdbdd99bad8
zohaibskylite786 <109808839+zohaibskylite786@users.noreply.github.com>
2022-09-22 07:09:00 -0700

    lastest documention

---
+++ public/js/searchIndex.js
@@ -0,0 +1,14 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\LARAVEL_START",
+            "name": "LARAVEL_START",
+            "summary": "",
+            "url": "namespaces/default.html#constant_LARAVEL_START"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
